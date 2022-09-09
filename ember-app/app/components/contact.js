1f6b00644954570e5c71058d446e55d38d48483a
amiar <amiarslimane@yandex.ru>
2022-09-09 09:56:25 +0100

    add toast

--- ember-app/app/components/contact.js
+++ ember-app/app/components/contact.js
@@ -1,9 +1,22 @@
 import Component from '@glimmer/component';
 import { action } from '@ember/object';
+import { inject as service } from '@ember/service';
 
 export default class ContactComponent extends Component {
+
+  @service euiToaster;
+
+
+
   @action
   submit(evt) {
     evt.preventDefault();
+    this.euiToaster.show({
+      title: `Not yet implemented`,
+      body: 'Please use the email address instead, waiting for this feature to be ready',
+      color: 'warning',
+      iconType: 'clock',
+      toastLifeTimeMs: 15000
+    });
   }
 }
