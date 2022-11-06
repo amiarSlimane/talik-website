e11d8fb509f4d5cf7bc844344f3d1e729f3e6ecd
amiar <amiarslimane@yandex.ru>
2022-11-06 07:56:57 +0100

    prettier format

--- ember-app/app/routes/index.js
+++ ember-app/app/routes/index.js
@@ -17,16 +17,17 @@ export default class IndexRoute extends Route {
     });
   }
 
-
   afterModel() {
     this.headData.title = 'Talik.io comments integration platform';
-    this.headData.keywords = "talik comment integration platform opensource free easy ";
+    this.headData.keywords =
+      'talik comment integration platform opensource free easy ';
     this.headData.language = 'English';
-    this.headData.description = 'Talik is an open source platform that provides an easy and efficient way to integrate comments on your web pages, and allow people to ask questions or give feedback. ';
+    this.headData.description =
+      'Talik is an open source platform that provides an easy and efficient way to integrate comments on your web pages, and allow people to ask questions or give feedback. ';
     this.headData.ogImage = 'https://talik.io/assets/images/header-image.svg';
-    this.headData.ogDescription = 'Talik is an open source platform that provides an easy and efficient way to integrate comments on your web pages, and allow people to ask questions or give feedback.';
+    this.headData.ogDescription =
+      'Talik is an open source platform that provides an easy and efficient way to integrate comments on your web pages, and allow people to ask questions or give feedback.';
     this.headData.ogType = 'website';
     this.headData.ogUrl = 'https://talik.io';
-
   }
 }
