3a04d20015294f34b880c65e5223117572fe3e61
amiar <amiarslimane@yandex.ru>
2022-11-06 15:11:06 +0100

    remove scroll to anchor from localstorage on app boot

--- ember-app/app/controllers/application.js
+++ ember-app/app/controllers/application.js
@@ -3,7 +3,13 @@ import { inject as service } from '@ember/service';
 
 export default class ApplicationController extends Controller {
   @service router;
+  @service fastboot;
 
+  constructor(...args){
+    super(...args);
+    if(!this.fastboot.isFastBoot)
+    window.localStorage.removeItem('anchor')
+  }
   get isAdmin() {
     const url = this.router.currentURL;
     if (url.includes('/admin')) return true;
