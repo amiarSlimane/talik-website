4f113ead67b4ee43f9cd84398fd14cb995c95f3e
amiar <amiarslimane@yandex.ru>
2022-11-06 15:11:31 +0100

    comment scroll events

--- ember-app/app/modifiers/scroll-anchor.js
+++ ember-app/app/modifiers/scroll-anchor.js
@@ -8,5 +8,42 @@ export default class ScrollAnchorModifier extends Modifier {
     } else {
       window.location.hash = '#home';
     }
+    // let mouseX;
+    // let mouseY;
+    // document.addEventListener('mousemove', event => {
+    //   mouseX = event.clientX;
+    //   mouseY = event.clientY;
+    //   const scrollTarget = document.elementFromPoint(mouseX, mouseY);
+    //   if(scrollTarget==null)return;
+
+    //   console.log(scrollTarget.id);
+    //   switch(scrollTarget.id){
+    //     case 'home':
+    //       window.localStorage.setItem('anchor',scrollTarget.id )
+    //       break;
+    //     case 'features':
+    //       window.localStorage.setItem('anchor',scrollTarget.id )
+    //       break;
+    //     case 'contact':
+    //       window.localStorage.setItem('anchor',scrollTarget.id )
+    //       break;
+    //   }
+    // });
+
+    // document.addEventListener("scroll", (event)=>{
+    //    const scrollTarget = document.elementFromPoint(mouseX, mouseY);
+    //    console.log('scrollTarget ',scrollTarget.id);
+    //     switch(scrollTarget.id){
+    //       case 'home':
+    //         window.localStorage.setItem('anchor',scrollTarget.id )
+    //         break;
+    //       case 'features':
+    //         window.localStorage.setItem('anchor',scrollTarget.id )
+    //         break;
+    //       case 'contact':
+    //         window.localStorage.setItem('anchor',scrollTarget.id )
+    //         break;
+    //     }
+    // });
   }
 }
